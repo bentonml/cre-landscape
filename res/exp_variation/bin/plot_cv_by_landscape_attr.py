131a5ccffa6d214c935dfaf49839d63edbfb70e5
bentonml <marylaurenbenton@gmail.com>
2022-05-02 16:34:21 -0500

    updates to figures (with combined heatmap)

--- res/exp_variation/bin/plot_cv_by_landscape_attr.py
+++ res/exp_variation/bin/plot_cv_by_landscape_attr.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 
 from datetime import date
 from scipy.stats import spearmanr
+import statsmodels.api as sm
 
 
 plt.switch_backend('agg')  # add to save plots non-interactively
@@ -70,26 +71,34 @@ def read_cv_data(tis, thresh='0.8'):
 for landscape_def in ['loop', 'contact']:
     print(landscape_def)
 
+    # create dataframe of enhancer number by gene with all tissues
+    all_tis = read_data(landscape_def)
+
     for tis in tis_order:
         df = read_cv_data(tis)
-
-        # create dataframe of enhancer number by gene with all tissues
-        all_tis = read_data(landscape_def)
-        all_tis_exp = all_tis[all_tis['exp']==1]
-
-        df_merge = df.merge(all_tis_exp, left_on='Name', right_on='target_gene', how='inner')
+        tis_exp = all_tis[all_tis['exp']==1].query(f'tissue=="{tis}"')
+        df_merge = df.merge(tis_exp, left_on='Name', right_on='target_gene', how='inner', validate='1:1')
 
         with sns.plotting_context("paper", rc=rc):
-            g = sns.lmplot(x='median_log2_tpm', y='log2cv', data=df_merge, lowess=True, scatter_kws={'alpha':0.3, 'color':'.3'}, line_kws={'color':'tab:red'})
-            g.set_xlabels('Median log2 Expression')
-            g.set_ylabels('CV')
+            plt.figure(figsize=(4,4))
+            g = sns.regplot(x='median_log2_tpm', y='log2cv', data=df_merge,
+                           lowess=True, scatter_kws={'alpha':0.5, 'color':'.2', 's':1},
+                           line_kws={'color':'tab:red'})
+            plt.xlabel('log2(Expression)')
+            plt.ylabel('log2(CV)')
+            sns.despine()
             plt.tight_layout()
             plt.savefig(f'{RES_PATH}/{tis}_{landscape_def}_log2exp_v_cv_lmplot.{fmt}', format=fmt, dpi=400)
             plt.close()
 
-            g = sns.lmplot(x='median_log2_tpm', y='loess_resid', data=df_merge, lowess=True, scatter_kws={'alpha':0.3, 'color':'.3'}, line_kws={'color':'tab:red'})
-            g.set_xlabels('Median log2 Expression')
-            g.set_ylabels('Expression Variation')
+        with sns.plotting_context("paper", rc=rc):
+            plt.figure(figsize=(4,4))
+            g = sns.regplot(x='median_log2_tpm', y='loess_resid', data=df_merge,
+                           lowess=False, scatter_kws={'alpha':0.5, 'color':'.2', 's':1},
+                           line_kws={'color':'tab:red'}, ci=False)
+            plt.xlabel('log2(Expression)')
+            plt.ylabel('Expression Variation')
+            sns.despine()
             plt.tight_layout()
             plt.savefig(f'{RES_PATH}/{tis}_{landscape_def}_log2exp_v_expVar_lmplot.{fmt}', format=fmt, dpi=400)
             plt.close()
@@ -130,3 +139,36 @@ for landscape_def in ['loop', 'contact']:
                 corr_p = gt0.filter(['Expression variation', 'Tissue-specificity (gene)', '# CREs', '% tissue-specific CREs', '% PhastCons']).corr(method=spearman_pval)
                 print(corr_p)
 
+
+for landscape_def in ['loop', 'contact']:
+    dfs = []
+    # create dataframe of enhancer number by gene with all tissues
+    all_tis = read_data(landscape_def)
+
+    # create dataframe of all tissues
+    for tis in tis_order:
+        df = read_cv_data(tis)
+        tis_exp = all_tis[all_tis['exp']==1].query(f'tissue=="{tis}"')
+        df_merge = df.merge(tis_exp, left_on='Name', right_on='target_gene', how='inner', validate='1:1')
+        df_merge = df_merge.rename(columns={'loess_resid':'Expression variation', 'rel_entropy':'Tissue-specificity (gene)',
+                                    'enh_num':'# CREs', 'frac_tisspec_enh':'% tissue-specific CREs', 'frac_phastcons':'% PhastCons'})
+        corr = df_merge.filter(['Expression variation', 'Tissue-specificity (gene)', '# CREs', '% tissue-specific CREs', '% PhastCons']).corr('spearman')
+        d = pd.DataFrame(corr['Expression variation']).T.assign(Tissue=tis)
+        dfs.append(d)
+
+    # combine all tissues
+    df_corr = pd.concat(dfs).set_index('Tissue').drop(columns='Expression variation')
+
+
+    # plot heatmap of all tissues
+    with sns.plotting_context("paper", rc=rc):
+        f, ax = plt.subplots(figsize=(12, 9))
+        cmap = sns.diverging_palette(240, 10, as_cmap=True)
+        g = sns.heatmap(df_corr, cmap=cmap, vmax=1, center=0, vmin=-1,
+                        square=True, linewidths=.5, cbar_kws={"shrink": .5}, annot=True)
+        g.set_xticklabels(g.get_xticklabels(), rotation = 30, horizontalalignment='right')
+        g.set_yticklabels(tis_names)
+        g.set_ylabel('')
+        plt.tight_layout()
+        plt.savefig(f'{RES_PATH}/{landscape_def}_expVar_heatmap_alltis.{fmt}', format=fmt, dpi=400)
+        plt.close()
